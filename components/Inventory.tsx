5988eb43ced654442899bb87509919f082071cd4
Muhammad Nuruddin <nuruddiniuk@gmail.com>
2026-02-14 12:09:53 +0600

    feat: Add React app entry point and clean up Inventory component
    Includes the main `<script type="module" src="./index.tsx"></script>` tag in `index.html` to bootstrap the React application.
    
    Additionally, removes an unused empty line at the end of the `Inventory.tsx` component file.

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useMemo, useRef, useEffect } from 'react';
 import { BusinessState, Language, Product } from '../types';
 import { TRANSLATIONS, PRODUCT_CATEGORIES } from '../constants';
